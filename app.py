6592a19ce05466ab32b6025471e9abd3de538199
S.M. Khalid Syfullah <syfullah.shifat@gmail.com>
2024-11-08 23:15:54 +0600

    added ipl match predictor

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ st.set_page_config(
 #nav
 selected = option_menu(
     menu_title=None,
-    options=['Home', 'Guide', 'T20', 'BPL', 'About'],
-    icons=['bi bi-house', 'bi bi-file-earmark-fill', 'bi bi-cpu', 'bi bi-cpu', 'bi bi-exclamation-square'],
+    options=['Home', 'Guide', 'T20', 'BPL', 'IPL', 'About'],
+    icons=['bi bi-house', 'bi bi-file-earmark-fill', 'bi bi-cpu', 'bi bi-cpu', 'bi bi-cpu', 'bi bi-exclamation-square'],
     default_index=0,
     orientation='horizontal',
     )
@@ -39,7 +39,7 @@ if selected == 'Home':
 # guide
 if selected == 'Guide':
     guide = st.selectbox('Select model for it\'s guide:',
-                        ('T20 International Model', 'BPL Model'))
+                        ('T20 International Model', 'BPL Model',  'IPL Model'))
 
     if guide == 'T20 International Model':
         st.html('''
@@ -47,7 +47,7 @@ if selected == 'Guide':
                         ''')
         st.markdown(
             '''
-            **Choose Prediction Type:** Select either `1st innings Projected Score` or `Win probability based on 2nd innings`.
+            1. **Choose Prediction Type:** Select either `1st innings Projected Score` or `Win probability based on 2nd innings`.
     
             2. Entering Match Details
             - Teams: Choose the batting and bowling teams from the dropdown lists.
@@ -76,7 +76,32 @@ if selected == 'Guide':
                         ''')
         st.markdown(
             '''
-            **Choose Prediction Type:** Select either `1st innings Projected Score` or `Win probability based on 2nd innings`.
+
+            1. Entering Match Details
+            - Teams: Choose the batting and bowling teams from the dropdown lists.
+            - Venue: Select the match venue from the provided options. 
+            2. Input Match Stats: 
+            - Overs completed: Number of overs completed.
+            - Balls bowled: Total balls bowled so far.
+            - Current runs: Runs scored by the batting team.
+            - Wickets: Number of wickets lost by the batting team.
+            - Last Five Overs Stats:
+                - Runs scored in last five overs.
+                - Wickets gone in last five overs.
+            3. Generate Prediction
+            - Click on `Predict`: After entering all inputs, click `Predict` to generate the prediction.
+                - Projected Score (1st innings): Displays the estimated total score the batting team may achieve.
+
+            `Note`: For predicting `1st innings Projected Score` of any match, you have the the match data till 5 overs at least.  
+            '''
+        )
+    if guide == 'IPL Model':
+        st.html('''
+                                <h2>Guide for T20 International Match Prediction in CricPred</h2>
+                                ''')
+        st.markdown(
+            '''
+            1. **Choose Prediction Type:** Select either `1st innings Projected Score` or `Win probability based on 2nd innings`.
 
             2. Entering Match Details
             - Teams: Choose the batting and bowling teams from the dropdown lists.
@@ -86,12 +111,15 @@ if selected == 'Guide':
             - Balls bowled: Total balls bowled so far.
             - Current runs: Runs scored by the batting team.
             - Wickets: Number of wickets lost by the batting team.
-            - Last Five Overs Stats:
+            - Last Five Overs Stats (`1st innings Projected Score`):
                 - Runs scored in last five overs.
                 - Wickets gone in last five overs.
-            4. Generate Prediction
+            4. For the `Win probability based on 2nd innings` option, additional inputs include:
+            - Target: The target score set by the first team.
+            5. Generate Prediction
             - Click on `Predict`: After entering all inputs, click `Predict` to generate the prediction.
                 - Projected Score (1st innings): Displays the estimated total score the batting team may achieve.
+                - Win Probability (2nd innings): Shows the probability of either team winning based on the current match situation.
 
             `Note`: For predicting `1st innings Projected Score` of any match, you have the the match data till 5 overs at least.  
             '''
@@ -437,6 +465,170 @@ if selected == 'BPL':
 
             st.subheader(f'Projected Score: {round(pred_score[0])}', anchor=False)
 
+# IPL
+if selected == 'IPL':
+    st.html('''
+                <h2>Indian Premier League(IPL) Match Predictor</h2>
+            ''')
+    teams = ['Royal Challengers Bengaluru', 'Kolkata Knight Riders',
+             'Delhi Capitals', 'Sunrisers Hyderabad', 'Mumbai Indians',
+             'Punjab Kings', 'Gujarat Titans', 'Chennai Super Kings',
+             'Rajasthan Royals', 'Lucknow Super Giants']
+
+    venue = ['Arun Jaitley Stadium, Delhi',
+             'Barabati Stadium',
+             'Barsapara Cricket Stadium, Guwahati',
+             'Bharat Ratna Shri Atal Bihari Vajpayee Ekana Cricket Stadium, Lucknow',
+             'Brabourne Stadium, Mumbai',
+             'De Beers Diamond Oval',
+             'Dr DY Patil Sports Academy, Mumbai',
+             'Dr. Y.S. Rajasekhara Reddy ACA-VDCA Cricket Stadium, Visakhapatnam',
+             'Eden Gardens, Kolkata',
+             'Feroz Shah Kotla',
+             'Himachal Pradesh Cricket Association Stadium, Dharamsala',
+             'Holkar Cricket Stadium',
+             'JSCA International Stadium Complex',
+             'M Chinnaswamy Stadium, Bengaluru',
+             'MA Chidambaram Stadium, Chepauk, Chennai',
+             'Maharaja Yadavindra Singh International Cricket Stadium, Mullanpur',
+             'Maharashtra Cricket Association Stadium, Pune',
+             'Narendra Modi Stadium, Ahmedabad',
+             'Punjab Cricket Association IS Bindra Stadium, Mohali, Chandigarh',
+             'Rajiv Gandhi International Stadium, Uppal, Hyderabad',
+             'Sardar Patel Stadium, Motera',
+             'Saurashtra Cricket Association Stadium',
+             'Sawai Mansingh Stadium, Jaipur',
+             'Shaheed Veer Narayan Singh International Stadium',
+             'Subrata Roy Sahara Stadium',
+             'Vidarbha Cricket Association Stadium, Jamtha',
+             'Wankhede Stadium, Mumbai']
+
+
+    proj_prob = st.selectbox('Select which type of prediction you want to see:',
+                ('1st innings Projected Score', 'Win probability based on 2nd innings'))
+
+    st.markdown(f'Fill the input values for: `{proj_prob}`')
+
+    # 1st innings projected score
+    if proj_prob == '1st innings Projected Score':
+        col1, col2 = st.columns(2)
+        with col1:
+            batting_team = st.selectbox(
+                'Batting Team',
+                sorted(teams),
+            )
+        with col2:
+            bowling_team = st.selectbox(
+                'Bowling Team',
+                sorted(teams),
+            )
+
+
+        venue = st.selectbox(
+                'Venue',
+                venue,
+        )
+
+        col1, col2, col3, col4 = st.columns(4)
+        with col1:
+            over = st.number_input('Overs completed', format='%0.0f')
+        with col2:
+            ball = st.number_input('Balls bowled', format='%0.0f')
+        with col3:
+            wickets = st.number_input('Wickets', format='%0.0f')
+        with col4:
+            current_runs = st.number_input('Current runs', format='%0.0f')
+
+        col1, col2 = st.columns(2)
+        with col1:
+            last_five_runs = st.number_input('Runs scored in last five overs', format='%0.0f')
+        with col2:
+            last_five_wicket = st.number_input('Wickets gone in last five overs', format='%0.0f')
+
+        if st.button('Predict', type='primary'):
+            if batting_team != bowling_team:
+                balls_left = 120 - ((over * 6) + ball)
+                wickets_left = 10 - wickets
+                crr = (current_runs * 6) / ((over * 6) + ball)
+
+                df = pd.DataFrame({'batting_team':[batting_team],
+                                   'bowling_team':[bowling_team],
+                                   'venue':[venue],
+                                   'balls_left':[balls_left],
+                                   'wickets_left':[wickets_left],
+                                   'current_runs':[current_runs],
+                                   'current_run_rate':[crr],
+                                   'last_five_runs':[last_five_runs],
+                                   'last_five_wickets':[last_five_wicket]})
+
+                model = pickle.load(open('models/ipl_proj_model.pkl', 'rb'))
+                pred_score = model.predict(df)
+
+                st.subheader(f'Projected Score: {round(pred_score[0])}', anchor=False)
+
+    # win probability based on 2nd innings
+    if proj_prob == 'Win probability based on 2nd innings':
+        col1, col2 = st.columns(2)
+        with col1:
+            batting_team = st.selectbox(
+                'Batting Team',
+                sorted(teams),
+            )
+        with col2:
+            bowling_team = st.selectbox(
+                'Bowling Team',
+                sorted(teams),
+            )
+
+
+        venue = st.selectbox(
+                'Venue',
+                (venue),
+        )
+
+
+        col1, col2 = st.columns(2)
+        with col1:
+            target = st.number_input('Target', format='%0.0f')
+        with col2:
+            current_runs = st.number_input('Current runs', format='%0.0f')
+
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            over = st.number_input('Overs completed', format='%0.0f')
+        with col2:
+            ball = st.number_input('Balls bowled', format='%0.0f')
+        with col3:
+            wickets = st.number_input('Wickets', format='%0.0f')
+
+
+        if st.button('Predict', type='primary'):
+            if batting_team != bowling_team:
+                balls_left = 120 - ((over * 6) + ball)
+                wickets_left = 10 - wickets
+                crr = (current_runs * 6) / ((over * 6) + ball)
+                rrr = ((target - current_runs) * 6) / balls_left
+
+
+                df = pd.DataFrame({'batting_team':[batting_team],
+                                   'bowling_team':[bowling_team],
+                                   'venue':[venue],
+                                   'target':[target],
+                                   'balls_left':[balls_left],
+                                   'wickets_left':[wickets_left],
+                                   'current_runs':[current_runs],
+                                   'current_run_rate':[crr],
+                                   'required_run_rate':[rrr]})
+
+                model = pickle.load(open('models/ipl_prob_model.pkl', 'rb'))
+
+                t20_win_prob = model.predict_proba(df)
+                win = t20_win_prob[0][1] * 100
+                loss = t20_win_prob[0][0] * 100
+
+                st.subheader(f'{batting_team} : {round(win)}%', anchor=False)
+                st.subheader(f'{bowling_team} : {round(loss)}%', anchor=False)
+
 
 # About
 if selected == "About":
@@ -449,7 +641,7 @@ if selected == "About":
                 <h2>About The Models</h2>
             ''')
     model_info = st.selectbox('Select Model: ',
-                              ('T20 International Model', 'BPL Model'))
+                              ('T20 International Model', 'BPL Model', 'IPL Model'))
     if model_info == 'T20 International Model':
         st.markdown(
             '''
@@ -483,6 +675,27 @@ if selected == "About":
             
             - R² Score: 96.5% – This metric score signifies that `on average`, our model explains 96.5% of the variance in the data, reflecting strong predictive power and high model reliability.
             '''
+        )
+    if model_info == 'IPL Model':
+        st.markdown(
+            '''
+            `**` IPL Model: `1st Innings Projected Score`
+
+            After performing 10 cross-validations on the model, the following metrics have been achieved:
+
+            - Mean Absolute Error (MAE): 2.70 – This metric indicates that, `on average`, the model's predictions deviate from the actual values by about 2.70 units.
+
+            - Root Mean Squared Error (RMSE): 5.42 – This metric demonstrates that `on average`, model's predicted values differ from the actual values by 5.66 units.
+
+            - R² Score: 97% – This metric score signifies that `on average`, our model explains 97% of the variance in the data, reflecting strong predictive power and high model reliability.
+            
+            IPL Model: `Win Probability based on 2nd Innings`
+                    
+            After performing 10 cross-validations on the model, the following metrics have been achieved:
+            
+            - Accuracy Score: 82% – This metric indicates that, on average, the model correctly predicts the outcome in approximately 82 out of every 100 cases during the training phase.
+            '''
+
         )
 
 # Footer
